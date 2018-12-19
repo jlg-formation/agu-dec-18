d5f5127c117cf2c337aad6e2c5416e50800d9396
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-19 10:34:39 +0100

    validation directive

--- src/app/cnaf/nir-validator.directive.ts
+++ src/app/cnaf/nir-validator.directive.ts
@@ -16,6 +16,9 @@ export class NirValidatorDirective implements Validator {
     if (!/^[12]/.test(control.value)) {
       return { badgender: 'le numero de secu ne commence pas par 1 ou 2' };
     }
+    if (!/^[12]\d{2}/.test(control.value)) {
+      return { badyear: 'le numero de secu doit avoir une annee de naissance' };
+    }
     return null;
   }
 
