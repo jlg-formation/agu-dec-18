ad6fd2ba04ea5d824371531294bb9ed0d240acce
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-19 10:06:03 +0100

    ok

--- src/app/cnaf/nir-validator.directive.ts
+++ src/app/cnaf/nir-validator.directive.ts
@@ -1,10 +1,22 @@
 import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
 
 @Directive({
-  selector: '[appNirValidator]'
+  selector: '[nirValidator]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: NirValidatorDirective, multi: true }]
 })
-export class NirValidatorDirective {
+export class NirValidatorDirective implements Validator {
 
+  secret = 'this is my secret';
   constructor() { }
+
+  validate(control: AbstractControl): { [key: string]: any } | null {
+    console.log('control', control.value);
+    // teste nir demarre par 1 ou 2.
+    if (!/^[12]/.test(control.value)) {
+      return { badgender: 'le numero de secu ne commence pas par 1 ou 2' };
+    }
+    return null;
+  }
 
 }
