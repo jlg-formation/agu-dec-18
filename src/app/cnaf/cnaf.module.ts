dd508943957463e2e2453467c6d1fa131ce6f805
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-19 11:19:03 +0100

    ok

--- src/app/cnaf/cnaf.module.ts
+++ src/app/cnaf/cnaf.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormatPhonePipe } from './format-phone.pipe';
 import { SwitchComponent } from './switch/switch.component';
 import { NirValidatorDirective } from './nir-validator.directive';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [FormatPhonePipe, SwitchComponent, NirValidatorDirective],
+  declarations: [FormatPhonePipe, SwitchComponent, NirValidatorDirective, LoginComponent],
   imports: [
     CommonModule
   ],
-  exports: [FormatPhonePipe, SwitchComponent, NirValidatorDirective]
+  exports: [FormatPhonePipe, SwitchComponent, NirValidatorDirective, LoginComponent]
 })
 export class CnafModule { }
